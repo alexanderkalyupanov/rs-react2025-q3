ea1f34681a2b03ffb313373a1461d028d52eb836
Alexander Kalyupanov <alexander.kalyupanov@mail.ru>
2025-07-25 00:03:49 +0300

    fix: fix tests errorBoundary

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -23,7 +23,7 @@ function App() {
   const [lastSearch, setLastSearch] = useState(
     localStorage.getItem('lastSearch') || ''
   );
-  // const [shouldThrow, setShouldThrow] = useState(false);
+  const [shouldThrow, setShouldThrow] = useState(false);
 
   useEffect(() => {
     fetchData(lastSearch);
@@ -51,9 +51,9 @@ function App() {
     setLastSearch(query);
   }, []);
 
-  // function triggerError(): void {
-  //   setShouldThrow(true);
-  // }
+  function triggerError(): void {
+    setShouldThrow(true);
+  }
 
   return (
     <ErrorBoundary>
@@ -80,6 +80,7 @@ function App() {
             <Route path="*" element={<NotFoundPage></NotFoundPage>}></Route>
           </Routes>
         </div>
+        <button onClick={triggerError}></button>
       </BrowserRouter>
     </ErrorBoundary>
   );
